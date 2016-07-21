4cf4ff2977b062a92c0b57075144794fe4b72ff4
Mathias Sundholm <mathias_sundholm@hotmail.com>
2016-07-21 23:31:49 +0200

    cleaned up code abit

--- image_stacking.py
+++ image_stacking.py
@@ -3,12 +3,16 @@ import cv2
 import numpy as np
 from time import time
 
-folder = "images/set3"
-file_list = os.listdir(folder)
-file_list = [os.path.join(folder, x) for x in file_list if x.endswith(".jpg")]
+image_folder = "images/"
+
+#
+file_list = os.listdir(image_folder)
+file_list = [os.path.join(image_folder, x) for x in file_list if x.endswith(".jpg")]
 
 fast = cv2.FastFeatureDetector_create()
 orb = cv2.ORB_create()
+
+show_debug = True
 
 # disable OpenCL to fix buggy ORB in OpenCV 3.1
 cv2.ocl.setUseOpenCL(False)
@@ -30,7 +34,7 @@ for file in file_list:
     matcher = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
 
     if avg_image is None:
-        # save keypoints for first image
+        # Save keypoints for first image
         avg_image = imageF
         first_image = image
         first_kp = kp
@@ -40,32 +44,33 @@ for file in file_list:
         matches = matcher.match(first_des, des)
         matches = sorted(matches, key=lambda x: x.distance)
 
-        # print(matches)
-        
+        # draw matches for debugging
         image_matches = image.copy()
-
         image_matches = cv2.drawMatches(first_image, first_kp, image, kp, matches, image_matches, flags=2)
 
         src_pts = np.float32([first_kp[m.queryIdx].pt for m in matches]).reshape(-1, 1, 2)
         dst_pts = np.float32([kp [m.trainIdx].pt for m in matches]).reshape(-1, 1, 2)
 
-
+        # Estimate perspective transformation
         M, mask = cv2.findHomography(dst_pts,src_pts,cv2.RANSAC,5.0)
         
+        # Compensate image for movements
+        w, h, _ = imageF.shape
 
-        corrected_image = cv2.warpPerspective(imageF ,M, (image.shape[1], image.shape[0]))
-
-        cv2.imshow("corrected", corrected_image)
+        corrected_image = cv2.warpPerspective(imageF ,M, (h,w))
+        
         avg_image += corrected_image
-    	#cv2.imshow("image_matches", image_matches)
+
+        if show_debug:
+            cv2.imshow("corrected", corrected_image)
+            #cv2.imshow("image_matches", image_matches)
+            cv2.waitKey(0)
 
     image = cv2.drawKeypoints(image, kp, image, color=(255, 0, 0))
 
-    #cv2.imshow("image", image)
-    #cv2.waitKey(0)
-
-
 avg_image /= len(file_list)
+
 print("Stacking " + str(len(file_list)) + " images in " + str(time()-tic) + " seconds")
-cv2.imshow("avg_image", avg_image)
+cv2.imshow("Stacked image", avg_image)
 cv2.waitKey(0)
+cv2.imwrite("result.jpg",(avg_image*255).astype(np.uint8))
